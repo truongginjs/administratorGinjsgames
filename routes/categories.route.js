82f894de39f92b4a8a73410a4a5dd677d2533e61
truongginjs <truongginjs@gmail.com>
2019-06-05 21:33:49 +0700

    fix chút chút

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -3,19 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/',(req,res)=>{
-    var active=[false,false,false,false,false];
-    active[2]=true;
-    res.render('vCategories/all',{
-        x: active
-    });
+    res.render('vCategories/all');
 });
 
 router.get('/all',(req,res)=>{
-    var active=[false,false,false,false,false];
-    active[2]=true;
-    res.render('vCategories/all',{
-       X: active
-    });
+    res.render('vCategories/all');
 });
 
 module.exports = router;
