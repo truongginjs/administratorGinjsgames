8e858c70c4034fd94d8c7050026b764c3870b941
Nguyễn Anh Tuấn <nguyenanhtuan6955@gmail.com>
2019-06-05 22:23:05 +0700

    Revert "cm"
    This reverts commit db00699b0f3962eb1dd4892cc40b34d99a763888, reversing
    changes made to 0279db90e226244be96fbb21ca2a911a6783817e.

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,12 +2,12 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("vProducts/all");
+router.get('/',(req,res)=>{
+    res.render('vProducts/all');
 });
 
-router.get("/all", (req, res) => {
-  res.render("vProducts/all");
+router.get('/all',(req,res)=>{
+    res.render('vProducts/all');
 });
 
 module.exports = router;
