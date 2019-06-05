624728a2667fc6db62981786c8dead1efa4c1dd7
Nguyễn Anh Tuấn <nguyenanhtuan6955@gmail.com>
2019-06-05 22:25:43 +0700

    cm1

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -3,11 +3,19 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.render('vCategories/all');
+    var active=[false,false,false,false,false];
+    active[2]=true;
+    res.render('vCategories/all',{
+        x: active
+    });
 });
 
 router.get('/all',(req,res)=>{
-    res.render('vCategories/all');
+    var active=[false,false,false,false,false];
+    active[2]=true;
+    res.render('vCategories/all',{
+       X: active
+    });
 });
 
 module.exports = router;
