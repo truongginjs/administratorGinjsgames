dde952f5245d56bd787e711631d49c92d6d5dc75
Nguyễn Anh Tuấn <nguyenanhtuan6955@gmail.com>
2019-06-05 22:22:33 +0700

    Revert "Merge branch 'master' of https://github.com/truongginjs/administratorGinjsgames"
    This reverts commit 17a37512e4aeca6dfca2564d2b2f3e7ba611e95b, reversing
    changes made to db00699b0f3962eb1dd4892cc40b34d99a763888.

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -2,13 +2,28 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/',(req,res)=>{
+<<<<<<< HEAD
     res.render('vCategories/all');
 });
 
-router.get('/all', (req, res) => {
+router.get('/all',(req,res)=>{
     res.render('vCategories/all');
+=======
+    var active=[false,false,false,false,false];
+    active[2]=true;
+    res.render('vCategories/all',{
+        x: active
+    });
+});
 
+router.get('/all',(req,res)=>{
+    var active=[false,false,false,false,false];
+    active[2]=true;
+    res.render('vCategories/all',{
+       X: active
+    });
+>>>>>>> acb69ad18e937645abafd416206a9736ab49a056
 });
 
 module.exports = router;
